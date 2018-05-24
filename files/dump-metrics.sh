b05e8e8708edded876fbfc95e804af6416284f5f
Katie Holly <git@meo.ws>
2018-05-24 09:41:52 +0200

    Feed query type stats into anyqast-pdns-recursor-querymetrics instead of anyqast-pdns-recursor-servermetrics

--- files/dump-metrics.sh
+++ files/dump-metrics.sh
@@ -4,4 +4,4 @@
 
 version=$(rec_control version)
 echo "anyqast-pdns-recursor-servermetrics,host=${HOSTNAME},version=${version} "$(rec_control get-all | awk '{print $1"="$2}' | tr '\n' ',' | head -c-1)
-rec_control get-qtypelist | awk "-vhostname=${HOSTNAME}" "-vversion=${version}" '{print "anyqast-pdns-recursor-servermetrics,host="hostname",version="version",qtype="$1" value="$2"i"}'
+rec_control get-qtypelist | awk "-vhostname=${HOSTNAME}" "-vversion=${version}" '{print "anyqast-pdns-recursor-querymetrics,host="hostname",version="version",qtype="$1" value="$2"i"}'
